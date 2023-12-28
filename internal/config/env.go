de6b2309f98b55327bfa08c5cfd5b435e4a2e4cd
fiqri khoirul m <fiqrikm18@gmail.com>
2023-12-28 07:09:47 +0700

    initial commit

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,5 +1,21 @@
 package config
 
-func NewAppConfig() {
+type AppConfigOpt func(opt *ConfigOpt)
 
+type ConfigOpt struct {
+	FilePath string
+}
+
+type AppConfig struct{}
+
+// NewAppConfig return all configuration from application
+func NewAppConfig(opts ...AppConfigOpt) *AppConfig {
+	return &AppConfig{}
+}
+
+// WithFilePath return function that inject value file path to the configuration
+func WithFilePath(path string) AppConfigOpt {
+	return func(opt *ConfigOpt) {
+		opt.FilePath = path
+	}
 }
